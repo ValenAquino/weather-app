86ab059b5f27246da85521251756a56ba9003bbb
ValenAquino <vaquino@frba.utn.edu.ar>
2023-01-10 16:41:28 -0300

    Proyecto Configurado

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname,'src','index.html'),
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+        filename: "style.css"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/i,
+        use: {
+            loader: "html-loader",
+            options: {
+                minimize: true
+            }
+        }
+      }
+    ],
+  },
+  devServer:{
+    port:5148,
+    open:true,
+    liveReload:true,
+    watchFiles: ["./src/*.html"]
+  }
+}
